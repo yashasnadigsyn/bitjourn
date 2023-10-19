b3f46860c6a7662b92972d2ebbf85cce5f3e5c60
s-m33r <112017240+s-m33r@users.noreply.github.com>
2023-10-19 16:06:02 +0530

    better templates, routes

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import w3storage
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
 
@@ -10,15 +11,22 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/create")
+def create_post():
+    return render_template("create.html")
 
 
 @app.route("/api/submit", methods=["POST"])
-def submit():
-    title = request.form["title"]
-    author = request.form["author"]
-    content = request.form["content"]
+def api_submit():
+    post_obj = {}
+    post_obj["title"] = request.form["title"]
+    post_obj["author"] = request.form["author"]
+    post_obj["content"] = request.form["content"]
+    post_obj["timestamp"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
     print(title, author, content)
 
